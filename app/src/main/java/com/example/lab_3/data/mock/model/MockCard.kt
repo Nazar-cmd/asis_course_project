80011f8993835d2b096d112c7cb282026fc2ac06
Nazar Velychko <nazar.v@radency.com>
2022-11-22 06:34:18 +0200

    updated mocks

--- app/src/main/java/com/example/lab_3/data/mock/model/MockCard.kt
+++ app/src/main/java/com/example/lab_3/data/mock/model/MockCard.kt
@@ -1,3 +1,12 @@
 package com.example.lab_3.data.mock.model
 
-data class MockCard(var name: String, var type: String, var img: String?)
+data class MockCard(
+    var name: String,
+    var type: String,
+    var img: String?,
+    var cost: Int,
+    var attack: Int,
+    var health: Int,
+    var rarity: String,
+    var cardSet: String,
+    )
