f16fdca359a65f904fc357edca9266451d5313ff
Nazar Velychko <nazar.v@radency.com>
2022-11-22 06:34:08 +0200

    added navigation

--- app/src/main/java/com/example/lab_3/MainActivity.kt
+++ app/src/main/java/com/example/lab_3/MainActivity.kt
@@ -3,22 +3,21 @@ package com.example.lab_3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.lab_3.ui.cards.CardsFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setFragment(CardsFragment())
-    }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-    private fun setFragment(homeFragment: Fragment) {
-
-        val fragmentManager = supportFragmentManager;
-        val fragmentTransaction = fragmentManager.beginTransaction()
-
-        fragmentTransaction.replace(R.id.frame_layout, homeFragment)
-        fragmentTransaction.commit()
+        navController = navHostFragment.navController
     }
 }
