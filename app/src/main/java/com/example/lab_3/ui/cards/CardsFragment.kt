29f8bcbbce65e2b6f9855aba6185d86b44f27ada
Nazar Velychko <nazar.v@radency.com>
2022-11-22 03:45:09 +0200

    refactored CardsFragment.kt

--- app/src/main/java/com/example/lab_3/ui/cards/CardsFragment.kt
+++ app/src/main/java/com/example/lab_3/ui/cards/CardsFragment.kt
@@ -1,6 +1,5 @@
 package com.example.lab_3.ui.cards
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -14,33 +13,15 @@ import com.example.lab_3.data.mock.model.MockCard
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 private lateinit var adapter: CardsAdapter
 private lateinit var recyclerView: RecyclerView
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CardsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CardsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+class CardsFragment : Fragment() {
     private var cardsArrayList = arrayListOf<MockCard>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
 
     }
 
@@ -50,26 +31,6 @@ class CardsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cards, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CardsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CardsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
